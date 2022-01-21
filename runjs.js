2fc2c8dc3ee06c1ca2a6f6ea9bd9ba18284220d2
Jonas L <1325712+JonL1@users.noreply.github.com>
2022-01-21 01:23:01 +0100

    Improvements

--- runjs.js
+++ runjs.js
@@ -1,20 +1,12 @@
-// TBD if requiring additional modules is a need for this custom extension
-// const { NodeVM } = require('vm2')
-// exports.run = async function (options) {
-//     options = this.parse(options)
-//     const data = options.data
-//     const vm = new NodeVM({ sandbox: { data } })
-//     try {
-//         return vm.run('module.exports =' + options.code)
-//     } catch (e) { console.error('Failed to execute script.', e); }
-// }
 const vm = require('vm')
 exports.run = async function (options) {
-    options = this.parse(options)
-    data = options.data || {}
+    const code = options.code
+    const data = this.parse(options.data) || {}
+    const timeout = this.parseOptional(options.timeout, 'number', 30000)
+    const show_errors = this.parseOptional(options.show_errors, 'boolean', false)
     vm.createContext(data)
     try {
-        const script = new vm.Script(options.code, "RunJS");
-        return script.runInContext(data);
-    } catch (e) { console.error('Failed to execute script.', e); }
+        const script = new vm.Script(code, "RunJS");
+        return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout });
+    } catch (e) { if (show_errors) console.error('Failed to execute script.', e); }
 }
