eb3ec3472cd3b62cae19b2961a66344fe576c6b6
Jonas L <1325712+JonL1@users.noreply.github.com>
2022-01-25 01:56:42 +0100

    breaking change: return

--- runjs.js
+++ runjs.js
@@ -1,12 +1,13 @@
 const vm = require('vm')
 exports.run = async function (options) {
-    const code = options.code
-    const data = this.parse(options.data) || {}
+    const code = `(function() { ${options.code} })()`
+    let data = this.parse(options.data) || {}
+    if (options.forward_console) data = { ...data, console: console }
+    vm.createContext(data)
     const timeout = this.parseOptional(options.timeout, 'number', 30000)
     const show_errors = this.parseOptional(options.show_errors, 'boolean', false)
-    vm.createContext(data)
     try {
         const script = new vm.Script(code, "RunJS");
-        return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout });
+        return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout })
     } catch (e) { if (show_errors) console.error('Failed to execute script.', e); }
 }
