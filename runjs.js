e0ac039e7489704b096d3ffc325194fdca5b2811
Jonas L <1325712+JonL1@users.noreply.github.com>
2022-07-12 15:59:12 +0200

    Hot fix for function declaration

--- runjs.js
+++ runjs.js
@@ -1,14 +1,14 @@
 const vm = require('vm')
 exports.run = async function (options) {
-  var code = `(${options.async_function ? 'async' : ''}function() { ${options.code} })()`
-  let data = this.parse(options.data) || {}
-  if (options.import_require) data = { ...data, require: require }
-  if (options.forward_console) data = { ...data, console: console }
-  vm.createContext(data)
-  const timeout = this.parseOptional(options.timeout, 'number', 30000)
-  const show_errors = this.parseOptional(options.show_errors, 'boolean', false)
-  try {
-    const script = new vm.Script(code, "RunJS");
-    return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout })
-  } catch (e) { if (show_errors) console.error('Failed to execute script.', e); }
+    var code = `(${options.async_function ? 'async' : ''} function() { ${options.code} } )()`
+    let data = this.parse(options.data) || {}
+    if (options.import_require) data = { ...data, require: require }
+    if (options.forward_console) data = { ...data, console: console }
+    vm.createContext(data)
+    const timeout = this.parseOptional(options.timeout, 'number', 30000)
+    const show_errors = this.parseOptional(options.show_errors, 'boolean', false)
+    try {
+        const script = new vm.Script(code, "RunJS");
+        return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout })
+    } catch (e) { if (show_errors) console.error('Failed to execute script.', e); }
 }
