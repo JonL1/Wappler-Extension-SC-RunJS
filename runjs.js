438c3f4da5637d4bd37bed83b936b6e0959a41d4
Jonas L <1325712+JonL1@users.noreply.github.com>
2022-11-28 13:58:06 +0100

    Option added to throw errors

--- runjs.js
+++ runjs.js
@@ -7,8 +7,12 @@ exports.run = async function (options) {
     vm.createContext(data)
     const timeout = this.parseOptional(options.timeout, 'number', 30000)
     const show_errors = this.parseOptional(options.show_errors, 'boolean', false)
+    const throw_errors = this.parseOptional(options.throw_errors, 'boolean', false)
     try {
         const script = new vm.Script(code, "RunJS");
         return await script.runInContext(data, { displayErrors: show_errors, timeout: timeout })
-    } catch (e) { if (show_errors) console.error('Failed to execute script.', e); }
+    } catch (e) { 
+        if (show_errors) console.error('Failed to execute script.', e);
+        if (throw_errors) throw e;
+    }
 }
